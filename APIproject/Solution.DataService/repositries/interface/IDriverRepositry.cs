e8d207e289b6420a8fddcdaab32256e395576c8b
Adebayo Awwal Bamidele <adebayoawwaldele@gmail.com>
2023-12-29 13:11:13 +0100

    add,update,Delete

---
+++ APIproject/Solution.DataService/repositries/interface/IDriverRepositry.cs
@@ -0,0 +1,9 @@
+﻿using Solution.Enitities.dbSet;
+
+namespace APIproject.Solution.DataService.Repositries.Interface
+{
+    public interface IDriverRepositry: IGenericRepositry<Driver>
+    {
+
+    }
+}
