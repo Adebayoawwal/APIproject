5cd7b8feed646408ef98c931bb777a0e65fe351c
Adebayo Awwal Bamidele <adebayoawwaldele@gmail.com>
2024-01-01 20:24:02 +0100

    Get,Post and Update

---
+++ APIproject/Solution.DataService/repositries/interface/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace APIproject.Solution.DataService.Repositries.Interface
+{
+    public interface IUnitOfWork
+  {
+        IDriverRepositry Drivers { get;  }
+        IAchievementRepositry Achievements { get; }
+
+
+
+        Task<bool> CompleteAsync();
+  }
+}
